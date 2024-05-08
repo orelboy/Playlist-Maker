f4f0edfbe1b919816b8e300d2abca623419f72fd
tsyplenkovdo <danil.ciplenkov@yandex.ru>
2024-05-08 10:55:03 +0300

    release

---
+++ app/src/main/java/com/practicum/playlist_maker/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.practicum.playlist_maker
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val buttonBack = findViewById<ImageView>(R.id.back)
+        val imageClickListener: View.OnClickListener = View.OnClickListener { finish() }
+        buttonBack.setOnClickListener(imageClickListener)
+    }
+}
