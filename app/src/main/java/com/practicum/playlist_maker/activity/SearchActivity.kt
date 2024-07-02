692d0e2623a2ee61d102247711f483bc32d9beaa
tsyplenkovdo <danil.ciplenkov@yandex.ru>
2024-07-02 21:21:14 +0300

    [sprint 11] Скрытие ошибок и выборки при пустой строке ввода

--- app/src/main/java/com/practicum/playlist_maker/activity/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker/activity/SearchActivity.kt
@@ -105,6 +105,10 @@ class SearchActivity : AppCompatActivity() {
                     }
 
                 })
+            }else {
+                errorInternet.isVisible = false
+                errorSearch.isVisible = false
+                rvTracksList.isVisible = false
             }
         }
 
