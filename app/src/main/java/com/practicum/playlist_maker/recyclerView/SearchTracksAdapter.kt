5e077a6de6f1fe8d43e26f5f02f37134ee0ca22e
tsyplenkovdo <danil.ciplenkov@yandex.ru>
2024-07-16 19:49:06 +0300

    12 sprint

--- app/src/main/java/com/practicum/playlist_maker/recyclerView/SearchTracksAdapter.kt
+++ app/src/main/java/com/practicum/playlist_maker/recyclerView/SearchTracksAdapter.kt
@@ -5,9 +5,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.practicum.playlist_maker.R
 
-class SearchTracksAdapter  (
-) : RecyclerView.Adapter<SearchTracksViewHolder> () {
+class SearchTracksAdapter : RecyclerView.Adapter<SearchTracksViewHolder> () {
     var tracks = ArrayList<Track>()
+    private var onClick: ((Track) -> Unit)? = null
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchTracksViewHolder {
@@ -17,9 +17,13 @@ class SearchTracksAdapter  (
 
     override fun onBindViewHolder(holder: SearchTracksViewHolder, position: Int) {
         holder.bind(tracks[position])
+        holder.itemView.setOnClickListener { onClick?.invoke(tracks[position]) }
     }
 
     override fun getItemCount(): Int {
         return tracks.size
+    }
+    fun setOnItemClickListener(listener: (Track) -> Unit) {
+        onClick = listener
     }
 }
--- app/src/main/java/com/practicum/playlist_maker/recyclerView/Track.kt
+++ app/src/main/java/com/practicum/playlist_maker/recyclerView/Track.kt
@@ -2,6 +2,7 @@ package com.practicum.playlist_maker.recyclerView
 import com.google.gson.annotations.SerializedName
 
 data class Track(
+    @SerializedName("trackId") val trackId: Int,
     @SerializedName("trackName") val trackName: String,
     @SerializedName("artistName") val artistName: String,
     @SerializedName("trackTimeMillis") val trackTime: Long,
