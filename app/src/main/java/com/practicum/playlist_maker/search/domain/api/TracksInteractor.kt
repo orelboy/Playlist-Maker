89fcc023b1a381de69de32a9c95cfafa8a891cc2
tsyplenkovdo <danil.ciplenkov@yandex.ru>
2024-11-15 01:24:18 +0300

    deleted the comment

--- app/src/main/java/com/practicum/playlist_maker/search/domain/api/TracksInteractor.kt
+++ app/src/main/java/com/practicum/playlist_maker/search/domain/api/TracksInteractor.kt
@@ -4,9 +4,5 @@ import com.practicum.playlist_maker.search.domain.models.SearchViewState
 import kotlinx.coroutines.flow.Flow
 
 interface TracksInteractor {
-//    fun interface TracksConsumer {
-//        fun consume(searchViewState: SearchViewState)
-//    }
-
     fun searchTracks(expression: String): Flow<SearchViewState>
 }
